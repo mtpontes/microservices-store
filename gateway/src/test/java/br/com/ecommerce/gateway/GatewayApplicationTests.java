cab5ef02ae0f9b14030a0e96f974fb2c685cecff
Mateus Pontes <mateuspsdd@gmail.com>
2024-09-24 17:18:29 -0300

    [gateway] test: fix

--- gateway/src/test/java/br/com/ecommerce/gateway/GatewayApplicationTests.java
+++ gateway/src/test/java/br/com/ecommerce/gateway/GatewayApplicationTests.java
@@ -2,8 +2,10 @@ package br.com.ecommerce.gateway;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = "api.security.gateway.name=gateway")
 class GatewayApplicationTests {
 
 	@Test
