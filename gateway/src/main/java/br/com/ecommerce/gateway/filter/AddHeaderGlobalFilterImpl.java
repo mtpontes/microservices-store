ffa25064854d64489af9fb676ffc6a5cb46aa1e1
Mateus Pontes <mateuspsdd@gmail.com>
2024-10-03 17:23:55 -0300

    [gateway] refactor: Add documentation and remove GlobalFilterImpl

--- gateway/src/main/java/br/com/ecommerce/gateway/filter/AddHeaderGlobalFilterImpl.java
+++ gateway/src/main/java/br/com/ecommerce/gateway/filter/AddHeaderGlobalFilterImpl.java
@@ -9,12 +9,30 @@ import org.springframework.web.server.ServerWebExchange;
 import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Mono;
 
+/**
+ * Global filter that intercepts all requests passing through the Gateway.
+ * 
+ * <p>
+ * The purpose of this class is to add the `"X-Forwarded-By"` header to all requests that pass through the Gateway,
+ * allowing subsequent microservices to identify that the request was forwarded by the company's internal Gateway
+ * application. This is particularly useful for endpoints that need to block external calls, such as those starting
+ * with "/internal", ensuring that only internal calls are accepted.
+ * </p>
+ *
+ * <p>
+ * The `"X-Forwarded-By"` header is dynamically set to the gateway value, defined by the property 
+ * `api.security.gateway.name` in the configuration file. This approach helps microservices differentiate
+ * internal calls from external ones and apply appropriate security policies, such as blocking external requests
+ * to certain endpoints.
+ * </p>
+ */
 @Slf4j
 @Component
 public class AddHeaderGlobalFilterImpl implements GlobalFilter {
 
     @Value("${api.security.gateway.name}")
     private String gatewayName;
+
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
