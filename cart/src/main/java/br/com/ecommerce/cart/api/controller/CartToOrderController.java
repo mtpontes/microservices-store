c0dcd5127f4a171257c3e3986242f0d02a2ec65a
Mateus Pontes <mateuspsdd@gmail.com>
2024-10-08 21:12:35 -0300

    [cart] refactor: Improve controller response handling

--- cart/src/main/java/br/com/ecommerce/cart/api/controller/CartToOrderController.java
+++ cart/src/main/java/br/com/ecommerce/cart/api/controller/CartToOrderController.java
@@ -4,6 +4,7 @@ import java.util.Set;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.com.ecommerce.cart.api.client.OrderClient;
 import br.com.ecommerce.cart.api.dto.order.OrderDataDTO;
+import br.com.ecommerce.cart.api.openapi.ICartToOrderController;
 import br.com.ecommerce.cart.infra.entity.Cart;
 import br.com.ecommerce.cart.infra.entity.Product;
 import br.com.ecommerce.cart.service.CartService;
@@ -25,7 +27,7 @@ import lombok.extern.slf4j.Slf4j;
 @RestController
 @RequestMapping("/cart-to-order")
 @AllArgsConstructor
-public class CartToOrderController {
+public class CartToOrderController implements ICartToOrderController {
 
     private final CartService service;
     private final OrderClient orderClient;
@@ -36,12 +38,11 @@ public class CartToOrderController {
         @AuthenticationPrincipal @Valid @NotNull UserDetailsImpl user,
         @RequestBody @Valid @NotEmpty Set<String> requestBody
     ) {
-        requestBody.stream()
-            .filter(productId -> !productId.isBlank())
-            .findFirst()
-            .orElseThrow(() -> new IllegalArgumentException("List of selected products is empty"));
+        Assert.isTrue(
+            requestBody.stream().anyMatch(productId -> !productId.isBlank()), 
+            "List of selected products has one or more blank references");
             
-        Cart cart = service.getUserCart(user.getId().toString());
+        Cart cart = service.getUserCart(user.getId());
         Set<Product> chosenProducts = service.selectProductsFromCart(cart, requestBody);
         OrderDataDTO orderData = orderClient.createOrder(user.getId(), chosenProducts);
         service.removeSelectedProducts(cart, chosenProducts);
--- cart/src/main/java/br/com/ecommerce/cart/api/controller/ClientCartController.java
+++ cart/src/main/java/br/com/ecommerce/cart/api/controller/ClientCartController.java
@@ -1,5 +1,6 @@
 package br.com.ecommerce.cart.api.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -7,10 +8,12 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import br.com.ecommerce.cart.api.dto.cart.AnonCartRefereceDTO;
 import br.com.ecommerce.cart.api.dto.cart.CartDTO;
 import br.com.ecommerce.cart.api.dto.cart.UpdateCartProductDTO;
+import br.com.ecommerce.cart.api.openapi.IClientCartController;
 import br.com.ecommerce.cart.api.view.CartViewModel;
 import br.com.ecommerce.cart.infra.entity.Cart;
 import br.com.ecommerce.cart.service.CartService;
@@ -23,38 +26,47 @@ import lombok.extern.slf4j.Slf4j;
 @RestController
 @RequestMapping("/carts")
 @AllArgsConstructor
-public class ClientCartController {
+public class ClientCartController implements IClientCartController {
 
     private final CartService service;
     private final CartViewModel cartViewModel;
 
 
     @PostMapping
-    public CartDTO create(@AuthenticationPrincipal UserDetailsImpl user) {
-        return service.createCart(user.getId().toString());
+    public ResponseEntity<CartDTO> create(
+        @AuthenticationPrincipal UserDetailsImpl user,
+        UriComponentsBuilder uriBuilder
+    ) {
+        CartDTO responseBody = service.createCart(user.getId());
+
+        var uri = uriBuilder
+            .path("/carts/{cartId}")
+            .buildAndExpand(responseBody.getId())
+            .toUri();
+        return ResponseEntity.created(uri).body(responseBody);
     }
 
     @GetMapping
-    public CartDTO get(@AuthenticationPrincipal UserDetailsImpl user) {
-        Cart cart = this.service.getUserCart(user.getId().toString());
-        return this.cartViewModel.getCartData(cart);
+    public ResponseEntity<CartDTO> get(@AuthenticationPrincipal UserDetailsImpl user) {
+        Cart cart = this.service.getUserCart(user.getId());
+        return ResponseEntity.ok(this.cartViewModel.getCartData(cart));
     }
 
     @PutMapping("/merge")
-    public CartDTO mergeCarts(
+    public ResponseEntity<CartDTO> mergeCarts(
         @AuthenticationPrincipal UserDetailsImpl user,
         @RequestBody @Valid AnonCartRefereceDTO requestBody
     ) {
-        Cart cart = this.service.mergeCart(user.getId().toString(), requestBody.getAnonCartId());
-        return this.cartViewModel.getCartData(cart);
+        Cart cart = this.service.mergeCart(user.getId(), requestBody.getAnonCartId());
+        return ResponseEntity.ok(this.cartViewModel.getCartData(cart));
     }
 
     @PutMapping
-    public CartDTO updateUnit(
+    public ResponseEntity<CartDTO> updateUnit(
         @AuthenticationPrincipal UserDetailsImpl user,
         @RequestBody @Valid UpdateCartProductDTO requestBody
     ) {
-        Cart cart = this.service.changeProductUnit(user.getId().toString(), requestBody);
-        return this.cartViewModel.getCartData(cart);
+        Cart cart = this.service.changeProductUnit(user.getId(), requestBody);
+        return ResponseEntity.ok(this.cartViewModel.getCartData(cart));
     }
 }
