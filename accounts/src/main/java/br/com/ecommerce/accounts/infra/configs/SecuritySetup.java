85843b48e2a897215287f6581eaee026d14cb85a
Mateus Pontes <mateuspsdd@gmail.com>
2024-10-09 08:14:03 -0300

    [accounts] refactor: Rename SpringConfig class to SecuritySetup

--- accounts/src/main/java/br/com/ecommerce/accounts/infra/configs/SecuritySetup.java
+++ accounts/src/main/java/br/com/ecommerce/accounts/infra/configs/SecuritySetup.java
@@ -8,7 +8,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
-public class SpringConfig {
+public class SecuritySetup {
 	
 	@Bean
 	public PasswordEncoder getPasswordEncoder() {
